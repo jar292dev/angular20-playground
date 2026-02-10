28ed93a8cda0f8430f149ff4ad0ac68ed7c14a94
jar292dev <jar292dev@gmail.com>
2026-02-10 19:14:46 +0100

    Crear index Ng Bootstrap

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -8,6 +8,7 @@ export const routes: Routes = [
             { path: 'angular', loadChildren: () => import('./features/angular/angular.routes').then(m => m.angularRoutes) },
             { path: 'formly', loadChildren: () => import('./features/tools/formly/formly.routes').then(m => m.formlyRoutes) },
             { path: 'zod', loadChildren: () => import('./features/tools/zod/zod.routes').then(m => m.zodRoutes) },
+            { path: 'ng-bootstrap', loadChildren: () => import('./features/ng-bootstrap/ng-bootstrap.routes').then(m => m.ngBootstrapRoutes) },
 
             { path: '**', redirectTo: 'not-found', pathMatch: 'full' },
         ]
