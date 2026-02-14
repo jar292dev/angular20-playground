8858e18fd4873d6e9e664455d8ef925f098c2674
jar292dev <jar292dev@gmail.com>
2026-02-14 22:29:37 +0100

    Pruebas lanzar y controlar errores

--- src/app/features/experimental/pages/index/index.ts
+++ src/app/features/experimental/pages/index/index.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, inject, output } from '@angular/core';
 import { Breadcrumbs } from "../../../../shared/components/breadcrumbs/breadcrumbs";
 import { OutputContainer } from "../../../../shared/components/output-container/output-container";
+import { NotFoundError, SystemError, VersionConflictError } from '../../../../core/errors/errors';
+import { TestErrorService } from '../../../angular/errores/services/test-error.service';
 
 @Component({
   selector: 'app-index',
@@ -9,6 +11,8 @@ import { OutputContainer } from "../../../../shared/components/output-container/
   styleUrl: './index.scss',
 })
 export class Index {
+  private testService = inject(TestErrorService);
+  protected output: any = null;
 
   // Migas de pan para la navegación
   breadcrumbs = [
@@ -16,4 +20,29 @@ export class Index {
     { label: 'Experimental' }
   ];
 
+  // === Métodos para lanzar errores manualmente ===
+  throwSystemError() {
+    throw new SystemError();
+  }
+
+  throwVersionConflictError() {
+    throw new VersionConflictError();
+  }
+
+  throwResourceNotFoundError() {
+    throw new NotFoundError('12345');
+  }
+
+  // === Métodos para probar llamadas HTTP ===
+  testApiCallWithCode(code: number) {
+    this.testService.testCode(code).subscribe({
+      next: (data) => {
+        this.output = data;
+      },
+      error: (error) => {
+        this.output = error;
+        throw error;
+      }
+    });
+  }
 }
